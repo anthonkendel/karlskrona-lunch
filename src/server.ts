d114f3b5d06c40115af81b276e2539834d8842c9
Anthon Holmqvist <anthon.kendel@gmail.com>
2019-02-23 00:14:27 +0100

    Setup linting with tslint

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
-import { exists } from "@server/utils/utils";
+import { exists } from '@server/utils/utils';
 
+// tslint:disable-next-line no-console
 console.log('server is executed', exists(['1', '2'], '1'));
